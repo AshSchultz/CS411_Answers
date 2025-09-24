1b40016bc4167f130a21c173a24a2e52efde0989
Ashley Schultz <akeragon907@gmail.com>
2025-09-23 17:27:10 -0800

    Done (Woooooo!)

--- exhaustive_search/bridges.cpp
+++ exhaustive_search/bridges.cpp
@@ -1,16 +1,36 @@
 #include "bridges.hpp"
 #include <iostream>
 
-int bridges(int w, int e, const vector<Bridge> & bridges) 
+int bridges_helper(const vector<Bridge> & bridges, vector<size_t> & chosen, size_t index)
 {
-	size_t size = bridges.size();
-	if (size < 1)
+	if (index >= bridges.size())
 	{
 		return 0;
-	} else if (size == 1)
-	{
-		return bridges[0][2];
 	}
+	int no_bridge_toll = bridges_helper(bridges, chosen, index+1);
+	Bridge curr = bridges[index];
 	
-	return 0;
+	for (auto bridge_index : chosen)
+	{
+		if ((curr[0] > bridges[bridge_index][0] && curr[1] < bridges[bridge_index][1])
+			|| (curr[0] < bridges[bridge_index][0] && curr[1] > bridges[bridge_index][1])
+			|| (curr[0] == bridges[bridge_index][0] || curr[1] == bridges[bridge_index][1]))
+		{
+			return no_bridge_toll;
+		}
+	}
+	// Trying the bridge
+	chosen.push_back(index);
+	int with_bridge_toll = bridges_helper(bridges, chosen, index+1);
+	with_bridge_toll += bridges[index][2];
+	chosen.pop_back();
+	return no_bridge_toll > with_bridge_toll ? no_bridge_toll : with_bridge_toll;
+}
+
+int bridges(int w, int e, const vector<Bridge> & bridges)
+{
+	// sort... please?
+	vector<size_t> chosen;
+	size_t index = 0; 
+	return bridges_helper(bridges, chosen, index);
 }

41dcaf384bb34e0ef41dee90848073c8962eb436
Ashley Schultz <akeragon907@gmail.com>
2025-09-25 15:26:05 -0800

    finalized code for submission

--- exhaustive_search/bridges.cpp
+++ exhaustive_search/bridges.cpp
@@ -1,14 +1,35 @@
+/**
+ * bridges.cpp
+ * Ashley Schultz
+ * 
+ * Solution for CS411 HW 2
+ * Source file for bridges function
+ */
+
 #include "bridges.hpp"
 /*
- * Takes two bridges east cities(a and c) and east cities (b and d)
- * then checks if the bridges overlap
- *  
+ * Takes two bridges west cities (a and c) and east cities (b and d)
+ * then checks if the bridges overlap.
 */ 
 inline bool bridge_invalid(int a, int b, int c, int d)
 {
 	return (((a>b) && (c<d)) || ((a<b) && (c>d)) || (a==b) || (c==d));
 }
 
+
+/**
+ * Recursive function that recursively checks
+ * all valid bridge combinations and finds the maximum toll.
+ * 
+ * @param index current index of the bridges array the function checks
+ * @param chosen chosen bridges in the current iteration
+ * @param bridges list of bridges, see main function
+ * 
+ * Preconditions:
+ * 	bridges is a vector of vectors of size 3
+ * 	chosen is empty initially
+ * 	index is zero initially
+ */
 int bridges_helper(const vector<Bridge> & bridges, vector<size_t> & chosen, size_t index)
 {
 	// BASE CASE
@@ -41,6 +62,23 @@ int bridges_helper(const vector<Bridge> & bridges, vector<size_t> & chosen, size
 	return no_bridge_toll > with_bridge_toll ? no_bridge_toll : with_bridge_toll;
 }
 
+/**
+ * bridges - wrapper for the recursive workhorse function
+ * Returns the maximum toll given a list of bridges
+ *
+ * 
+ * 	@param w number of west cities
+ * 	@param e number of east cities
+ * 	@param bridges vector of available bridges.
+ * 	
+ * a bridge is a list of length 3 with:
+ * 	  [west city, east city, toll] 
+ *  
+ * @pre
+ * 	bridges must have west and east cities < w and < e
+ *  bridges is a vector of vectors of size 3
+ * 
+ */
 int bridges(int w, int e, const vector<Bridge> & bridges)
 {
 	// Array of indecies of chosen bridges

8d9ec1b0d09a4284be16994de8f72497a241d59b
Ashley Schultz <akmooreschultz@alaska.edu>
2025-10-06 09:36:47 -0800

    Added initial implementation

--- divide_and_conquer/contigsum.hpp
+++ divide_and_conquer/contigsum.hpp
@@ -2,10 +2,36 @@
 #define CONTIGSUM
 
 #include <iterator>
+#include <vector>
+
+using std::vector;
+
+// Recursive work horse function
+template<typename RAIter>
+int contigSumRecurse(RAIter first, RAIter last, vector<RAIter> & chosen)
+{
+
+    // BASE CASE
+    long int size = last - first;
+    if (size == 0)
+    {
+        int sum = 0;
+        for (auto & e:chosen)
+        {
+            sum += *e;
+        }
+        return sum;
+    }
+    chosen.push_back(first);
+    return contigSumRecurse(first+1, last, chosen);
+    
+}
 
 template<typename RAIter>
 int contigSum(RAIter first, RAIter last) {
-    return 0;
+
+    vector<RAIter> chosen;
+    return contigSumRecurse(first, last, chosen);
 }
 
 #endif // #ifndef CONTIGSUM

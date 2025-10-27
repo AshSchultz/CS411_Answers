280fb90586d568f1f86d152157eceb6f1d6d07e0
Ashley Schultz <akmooreschultz@alaska.edu>
2025-10-27 12:32:21 -0800

    Applied formatting and linting to divide and conquer problems

--- divide_and_conquer/contigsum.hpp
+++ divide_and_conquer/contigsum.hpp
@@ -1,7 +1,7 @@
 /**
  * contigsum.hpp
  * by Ashley Schultz
- * 
+ *
  * Solution for homework 3 (divide and conquer)
  * Source for function contigsum
  * LAST MODIFIED: 2025-10-7
@@ -13,60 +13,50 @@
 #include <iterator>
 #include <vector>
 
-using std::vector;
 using std::max;
+using std::vector;
 
-
-// Recursive function that returns the Greatest Contiguous Sum (GCS) of a 
+// Recursive function that returns the Greatest Contiguous Sum (GCS) of a
 // sequence of integers.
 // Pre:
 //     [first, last) is a valid range.
-template<typename RAIter>
-int contigSum(RAIter first, RAIter last) {
-    size_t size = static_cast<size_t>(last - first);
-    // BASE CASE
-    if (size <= 1)
-    {
-        // Return zero if the first element doesn't exist or is less then zero
-        return size > 0 ? (*first > 0 ? *first : 0) : 0;
-    }
+template <typename RAIter> int contigSum(RAIter first, RAIter last) {
+  size_t size = static_cast<size_t>(last - first);
+  // BASE CASE
+  if (size <= 1) {
+    // Return zero if the first element doesn't exist or is less then zero
+    return size > 0 ? (*first > 0 ? *first : 0) : 0;
+  }
 
-    // RECURSIVE CASE
-    RAIter midpoint = first + (size/2);
-    int firstPartGCS = contigSum(first, midpoint);
-    int secondPartGCS = contigSum(midpoint, last);
+  // RECURSIVE CASE
+  RAIter midpoint = first + (size / 2);
+  int firstPartGCS = contigSum(first, midpoint);
+  int secondPartGCS = contigSum(midpoint, last);
 
-    //Find GCS with first value in the second part of the input
-    int firstSum = 0;
-    int GCSFirstSum = 0;
-    for (RAIter curr = midpoint+1; curr < last; curr++)
-    {
-        firstSum += *curr;
-        if (firstSum > GCSFirstSum)
-        {
-            GCSFirstSum = firstSum;
-        }
-        
+  // Find GCS with first value in the second part of the input
+  int firstSum = 0;
+  int GCSFirstSum = 0;
+  for (RAIter curr = midpoint + 1; curr < last; curr++) {
+    firstSum += *curr;
+    if (firstSum > GCSFirstSum) {
+      GCSFirstSum = firstSum;
     }
+  }
 
-    // Find GCS with last value in the first part of the input
-    int lastSum = 0;
-    int GCSLastSum = 0;
-    for (RAIter curr = first + (size/2); curr >= first; curr--)
-    {
-        lastSum += *curr;
-        if (lastSum > GCSLastSum)
-        {
-            GCSLastSum = lastSum;
-        }
-        
+  // Find GCS with last value in the first part of the input
+  int lastSum = 0;
+  int GCSLastSum = 0;
+  for (RAIter curr = first + (size / 2); curr >= first; curr--) {
+    lastSum += *curr;
+    if (lastSum > GCSLastSum) {
+      GCSLastSum = lastSum;
     }
-    
-    // Return the max of the first and second parts GCS,
-    // and the GCS of the first part with the last element and the
-    // second parts GCS with the first element
-    return max<int>({firstPartGCS, secondPartGCS, GCSLastSum + GCSFirstSum});
-    
+  }
+
+  // Return the max of the first and second parts GCS,
+  // and the GCS of the first part with the last element and the
+  // second parts GCS with the first element
+  return max<int>({firstPartGCS, secondPartGCS, GCSLastSum + GCSFirstSum});
 }
 
 #endif // #ifndef CONTIGSUM
--- divide_and_conquer/inversions.hpp
+++ divide_and_conquer/inversions.hpp
@@ -1,12 +1,13 @@
 /**
  * inversions.hpp
  * by Ashley Schultz
- * 
+ *
  * Solution for assignment 3 (divide and conquer)
- * 
+ *
  * mergeSort and stableMerge authored by Glen G. Chappell from
- * this link https://github.com/ggchappell/cs411-2025-03/blob/main/merge_sort.cpp
- * These functions were modified in the course of this assignment, modifications are
+ * this link
+ * https://github.com/ggchappell/cs411-2025-03/blob/main/merge_sort.cpp These
+ * functions were modified in the course of this assignment, modifications are
  * marked by the author.
  * LAST MODIFIED: 2025-10-7
  */
@@ -15,9 +16,9 @@
 #define INVERSIONS
 
 #include <iostream>
+using std::cin;
 using std::cout;
 using std::endl;
-using std::cin;
 #include <string>
 using std::string;
 using std::to_string;
@@ -29,10 +30,10 @@ using std::size_t;
 using std::move;
 #include <iterator>
 using std::begin;
-using std::end;
 using std::distance;
-using std::next;
+using std::end;
 using std::iterator_traits;
+using std::next;
 #include <algorithm>
 using std::move;
 using std::swap;
@@ -57,53 +58,50 @@ using std::abs;
 //     MODIFICATION:
 //     middle points to a value further in the list then first
 template <typename FDIter>
-size_t stableMerge(FDIter first, FDIter middle, FDIter last)
-{
-    // Get type of what iterators point to
-    using Value = typename iterator_traits<FDIter>::value_type;
+size_t stableMerge(FDIter first, FDIter middle, FDIter last) {
+  // Get type of what iterators point to
+  using Value = typename iterator_traits<FDIter>::value_type;
 
-    // MODIFICATION: add a static cast to size_t to silence compiler warning -Wsign-conversion
-    vector<Value> buffer(static_cast<size_t>(distance(first, last)));
-                               // Buffer for temporary copy of data
-    auto in1 = first;          // Read location in 1st half
-    auto in2 = middle;         // Read location in 2nd half
-    auto out = begin(buffer);  // Write location in buffer
+  // MODIFICATION: add a static cast to size_t to silence compiler warning
+  // -Wsign-conversion
+  vector<Value> buffer(static_cast<size_t>(distance(first, last)));
+  // Buffer for temporary copy of data
+  auto in1 = first;         // Read location in 1st half
+  auto in2 = middle;        // Read location in 2nd half
+  auto out = begin(buffer); // Write location in buffer
 
-    // Merge two sorted lists into a single list in buff.
-    // MODIFICATION: initialize variable to hold total inversions, to return later
-    size_t inversions = 0;
-    while (in1 != middle && in2 != last)
+  // Merge two sorted lists into a single list in buff.
+  // MODIFICATION: initialize variable to hold total inversions, to return later
+  size_t inversions = 0;
+  while (in1 != middle && in2 != last) {
+    if (*in2 < *in1) // Must do comparison this way, to be stable.
     {
-        if (*in2 < *in1)  // Must do comparison this way, to be stable.
-        {
-            // MODIFICATION: sum the number of inversions from each merge
-            inversions += static_cast<size_t>(distance(first, in2)-distance(begin(buffer), out));
-            *out++ = std::move(*in2++);
-        }
-        else
-        {
-            *out++ = std::move(*in1++);
-        }
+      // MODIFICATION: sum the number of inversions from each merge
+      inversions += static_cast<size_t>(distance(first, in2) -
+                                        distance(begin(buffer), out));
+      *out++ = std::move(*in2++);
+    } else {
+      *out++ = std::move(*in1++);
     }
+  }
 
-    // Move remainder of original sequence to buffer.
-    // Only one of the following two calls will do anything, since the
-    //  other is given an empty source range.
-    // NOTE. This is the 3-parameter version of std::move (the "move"
-    //  version of std::copy, declared in <algorithm>), not the
-    //  1-parameter version (casts to an Rvalue, declared in <utility>).
-    move(in1, middle, out);
-    move(in2, last, out);
+  // Move remainder of original sequence to buffer.
+  // Only one of the following two calls will do anything, since the
+  //  other is given an empty source range.
+  // NOTE. This is the 3-parameter version of std::move (the "move"
+  //  version of std::copy, declared in <algorithm>), not the
+  //  1-parameter version (casts to an Rvalue, declared in <utility>).
+  move(in1, middle, out);
+  move(in2, last, out);
 
-    // Move buffer contents back to original sequence location.
-    move(begin(buffer), end(buffer), first);
-    // MODIFICATION: return total number of inversions
-    return inversions;
+  // Move buffer contents back to original sequence location.
+  move(begin(buffer), end(buffer), first);
+  // MODIFICATION: return total number of inversions
+  return inversions;
 }
 
-
 // mergeSort
-// Sort a range using Merge Sort MODIFICATION: Returns the number 
+// Sort a range using Merge Sort MODIFICATION: Returns the number
 // of inversions from merging
 // Recursive.
 // Requirements on Types:
@@ -113,32 +111,30 @@ size_t stableMerge(FDIter first, FDIter middle, FDIter last)
 //     operator< is a total order on the value type of FDIter.
 // Pre:
 //     [first, last) is a valid range.
-template <typename FDIter>
-size_t mergeSort(FDIter first, FDIter last)
-{
-    // Compute size of range
-    auto size = distance(first, last);
+template <typename FDIter> size_t mergeSort(FDIter first, FDIter last) {
+  // Compute size of range
+  auto size = distance(first, last);
 
-    // BASE CASE
+  // BASE CASE
 
-    if (size <= 1)
-        return 0;
+  if (size <= 1)
+    return 0;
 
-    // RECURSIVE CASE
+  // RECURSIVE CASE
 
-    // Create iterator to middle of range
-    auto middle = next(first, size/2);
+  // Create iterator to middle of range
+  auto middle = next(first, size / 2);
 
-    // MODIFICATION: sum up total inversions from each recursive call
-    // and the final merge, store it in totalInversions
-    size_t totalInversions = 0;
-    // Recursively sort the two lists
-    totalInversions += mergeSort(first, middle);
-    totalInversions += mergeSort(middle, last);
-    // And merge them
-    totalInversions += stableMerge(first, middle, last);
-    // MODIFICATION: Return totalInversions
-    return totalInversions;
+  // MODIFICATION: sum up total inversions from each recursive call
+  // and the final merge, store it in totalInversions
+  size_t totalInversions = 0;
+  // Recursively sort the two lists
+  totalInversions += mergeSort(first, middle);
+  totalInversions += mergeSort(middle, last);
+  // And merge them
+  totalInversions += stableMerge(first, middle, last);
+  // MODIFICATION: Return totalInversions
+  return totalInversions;
 }
 
 // inversions
@@ -146,8 +142,7 @@ size_t mergeSort(FDIter first, FDIter last)
 // the number of inversions done by sorting the sequence
 // Pre:
 //     [first, last) is a valid range.
-template<typename RAIter>
-size_t inversions(RAIter first, RAIter last){
-    return mergeSort(first, last);
+template <typename RAIter> size_t inversions(RAIter first, RAIter last) {
+  return mergeSort(first, last);
 }
 #endif // #ifndef INVERSIONS

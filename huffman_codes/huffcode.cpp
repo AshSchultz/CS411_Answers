a1ce3be16f8818803e6e1d6add5620961af159c8
Ashley <akeragon907@gmail.com>
2025-11-25 10:52:12 -0900

    Cleaned up code for turning in

--- huffman_codes/huffcode.cpp
+++ huffman_codes/huffcode.cpp
@@ -1,6 +1,8 @@
-// huffcode.cpp  SKELETON
-// Glenn G. Chappell
-// 2025-11-10
+// huffcode.cpp
+//
+// Ashley Schultz
+// Glenn G. Chappell wrote the skeleton
+// 2025-11-25
 //
 // For CS 411 Fall 2025
 // Assignment 6, Exercise A
@@ -22,6 +24,8 @@ void HuffCode::setWeights(const unordered_map<char, int> &theweights) {
   std::priority_queue<shared_ptr<huffNode>, vector<shared_ptr<huffNode>>,
                       Compare>
       pq;
+
+  // Create leaf nodes
   for (auto weight : theweights) {
     auto node = make_shared<huffNode>();
     node->weight = weight.second;
@@ -29,12 +33,14 @@ void HuffCode::setWeights(const unordered_map<char, int> &theweights) {
     pq.push(node);
   }
 
+  // Create full tree
   while (pq.size() > 1) {
     auto l = pq.top();
     pq.pop();
     auto r = pq.top();
     pq.pop();
     auto newNode = make_shared<huffNode>();
+    // Set each non leaf node to null character
     newNode->character = '\0';
     newNode->left = l;
     newNode->right = r;
@@ -45,6 +51,7 @@ void HuffCode::setWeights(const unordered_map<char, int> &theweights) {
   src = pq.top();
 }
 
+// Internal workhorse function that recursively encodes a character using the huffman tree created in setWeights()
 string HuffCode::encodeChar(const char &a, shared_ptr<huffNode> tree,
                             string str, bool &found) const {
   string huffstring;
@@ -62,6 +69,7 @@ string HuffCode::encodeChar(const char &a, shared_ptr<huffNode> tree,
     return str;
   }
 
+  // Encode left subtree
   huffstring.append(encodeChar(a, tree->left, str + '0', found));
 
   if (found) {
@@ -69,6 +77,7 @@ string HuffCode::encodeChar(const char &a, shared_ptr<huffNode> tree,
     return huffstring;
   }
 
+  // Encode right subtree
   huffstring.append(encodeChar(a, tree->right, str + '1', found));
 
   if (found) {
@@ -99,6 +108,8 @@ string HuffCode::decode(const string &codestr) const {
   string decodedText;
 
   auto temp = src;
+  // Go through each binary '1' or '0' character, 
+  // traverse the tree depending on the character until a character is found
   for (size_t i = 0; i < codestr.length(); i++) {
     if (codestr[i] == '0') {
       temp = temp->left;
--- huffman_codes/huffcode.hpp
+++ huffman_codes/huffcode.hpp
@@ -1,6 +1,8 @@
-// huffcode.hpp  SKELETON
-// Glenn G. Chappell
-// 2025-11-10
+// huffcode.hpp
+//
+// Ashley Schultz
+// Glenn G. Chappell wrote the skeleton
+// 2025-11-25
 //
 // For CS 411 Fall 2025
 // Assignment 6, Exercise A
@@ -21,7 +23,6 @@
 // Class HuffCode
 // Encoding & decoding using a Huffman code
 class HuffCode {
-
   // ***** HuffCode: ctors, dctor, op= *****
 public:
   // Compiler-generated default ctor, copy ctor, copy =, dctor used
@@ -36,6 +37,8 @@ public:
 
   // ***** HuffCode: data members *****
 private:
+
+  // Node definition for the huffman tree
   struct huffNode {
     int weight;
     char character;
@@ -43,6 +46,7 @@ private:
     std::shared_ptr<huffNode> right = nullptr;
   };
 
+  // Class for comparing two nodes in priority queue, used internally in set weights function
   class Compare {
   public:
     bool operator()(std::shared_ptr<huffNode> a, std::shared_ptr<huffNode> b) {
@@ -50,10 +54,13 @@ private:
     };
   };
 
+  // Source for the huffman tree created in set weights function
   std::shared_ptr<huffNode> src;
 
+  // Map of characters to codes for encoding strings to use dynamic coding principles
   mutable std::unordered_map<char, std::string> huffMap;
 
+  // Internal workhorse function that recursively encodes a character using the huffman tree created in setWeights()
   std::string encodeChar(const char &a, std::shared_ptr<huffNode> tree,
                          std::string str, bool &found) const;
 
